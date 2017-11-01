6d6fecdffd2074230d7c49422cd81d5ac29fdece
Pradyumna chattopadhyay <chatterjee.pra@gmail.com>
2017-11-02 00:06:59 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -7,14 +7,20 @@ function show_callus_modal() {
   return false;
 }
 
- $( window ).resize(function() {
+function callus_switcher = function() {
   if($(window).width() <=768) { 
     $('.callus').addClass("callus-popup");
+    $(".callus-popup").on("click",show_callus_modal);
   } 
   else {
-    $('.callus').removeClass("callus-popup");
+    if($(".callus-popup").length){
+      $(".callus-popup").off("click");
+      $('.callus').removeClass("callus-popup");
+    }
   }
-});
+}
+
+$(document).ready(callus_switcher);
+$( window ).resize(callus_switcher);
 
 
-$(".callus-popup").click(show_callus_modal);
